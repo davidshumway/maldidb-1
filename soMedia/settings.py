ad60d418077f12e225d0c80cf951ac701df9f487
their <davidshumway@gmail.com>
2021-01-12 17:34:11 -0600

    Move sqlite to postgresql

--- soMedia/settings.py
+++ soMedia/settings.py
@@ -91,9 +91,17 @@ WSGI_APPLICATION = 'soMedia.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # ~ 'default': {
+        # ~ 'ENGINE': 'django.db.backends.sqlite3',
+        # ~ 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # ~ }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'maldidb',
+        'USER': 'maldiadmin',
+        'PASSWORD': '3SosVdNfJ9jKioOS4sWjdofji0zFpciVov',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 db_from_env = dj_database_url.config()
