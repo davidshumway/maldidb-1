0d5dbbc9703a4993c9b0c91342c9cf89b799ffa9
Ubuntu <dshumw2@uic.edu>
2021-02-09 05:08:55 +0000

    Update entrypoint.sh

--- maldidb/entrypoint.sh
+++ maldidb/entrypoint.sh
@@ -11,8 +11,7 @@ if [ "$DATABASE" = "postgres" ]; then
 fi
 
 # Make migrations and migrate the database.
-echo "Making migrations and migrating the database. (With makemigrations --merge) "
-python manage.py makemigrations --noinput --merge
+echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
