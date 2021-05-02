43ca5fc2beb453dc8dc7c3129500bee8a7ed4578
David Shumway <dshumw2@uic.edu>
2021-05-02 17:59:02 -0500

    Create serializers.py

---
+++ mdb/spectra/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+class CollapsedSpectraSerializer(serializers.ModelSerializer):
+  class Meta:
+  	model = CollapsedSpectra
+  	fields = '__all__'
+
+class SpectraSerializer(serializers.ModelSerializer):
+  class Meta:
+  	model = Spectra
+  	fields = '__all__'
