b6b7d652006dc48424d36d3684e993ba620067b0
davidshumway <davidshumway@gmail.com>
2021-07-12 23:40:39 -0500

    Updates docs

--- mdb/docs/admin.py
+++ mdb/docs/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 from .models import *
 
-admin.site.register(DocsPage, MarkdownxModelAdmin)
+class DocsPageAdmin(admin.ModelAdmin):
+  # a list of displayed columns name.
+  list_display = ['short_title', 'type', 'parent', 'order']
+  # ~ get_view_count.admin_order_field = 'parent'
+  ordering = ('type', 'parent', 'order')
+
+#admin.site.register(DocsPage, MarkdownxModelAdmin)
+admin.site.register(DocsPage, DocsPageAdmin)
